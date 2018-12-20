89452b15cc6ca2e1a2adef4a7e3ab383c936323f
DaniAntu <d.asenjo.c@gmail.com>
2018-12-20 11:14:26 -0300

    Agrega función que sube a todos los Pks

--- data.js
+++ data.js
@@ -1,8 +1,31 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+
+// window.dataPokemon = {
+
+  const example = () => {
   return 'example';
 };
 
-window.example = example;
+// window.example = example;
+
+const searchFor = (pokeId) => {
+  let arrayPokeID = POKEMON["pokemon"][pokeId];
+
+  return arrayPokeID;
+}
+
+const wholePokes = () => {
+  let arrayCompletePoke = POKEMON.pokemon;
+  return arrayCompletePoke;
+}
+
+const comparar = (pokeOne,pokeTwo) => {
+  //debe traer el id del pokemon a buscar
+  // POKEMON[]
+
+  return arrayPokes;
+}
+
+// }
