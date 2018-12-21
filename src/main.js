8bb749e002c4d54f22fce0c3c6428e37a8ce5bee
DaniAntu <d.asenjo.c@gmail.com>
2018-12-21 11:28:41 -0300

    Agrega imagenes dentro de div

--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
+window.onload = () => {
+    let arrayShowWhole = wholePokes();   
 
-let arrayShowWhole = wholePokes();
+    let docs = document.getElementById('root');
+    let user = document.getElementById('user');
+    const containment = document.getElementById('containmentPokes');
 
-console.log (arrayShowWhole);
+    //Ingreso con botón Aceptar
+    document.getElementById('btn_enter').addEventListener('click',(event) => {
+        event.preventDefault();
+        //Oculta el primer formulario
+        user.style.display = 'none';
 
+        const divPokemonGrid = document.createElement('div');
+        containment.appendChild(divPokemonGrid);
+        divPokemonGrid.setAttribute('id','divPokemon');
+        divPokemonGrid.classList.add("grid_container");
+
+        let img = [];
+        // document.getElementById('divPokemonGrid').innerHTML = '';
+        for (let i = 0 ; i < arrayShowWhole.length ; i++){
+            // console.log (arrayShowWhole);
+            img.push(arrayShowWhole[i].img);
+
+            const divImgPoke = document.createElement('div');
+            let imgElementPoke = document.createElement('img');
+            imgElementPoke.setAttribute('src',img[i]);
+            divPokemonGrid.appendChild(divImgPoke);
+            divImgPoke.appendChild(imgElementPoke);            
+        }
+
+    });
+
+
+//Los voy creando para agregar cada elemento
+// <div class="grid-container">
+//   <div class="grid-item">1</div>
+//   <div class="grid-item">2</div>
+//   <div class="grid-item">3</div>  
+//   <div class="grid-item">4</div>
+//   <div class="grid-item">5</div>
+//   <div class="grid-item">6</div>  
+//   <div class="grid-item">7</div>
+//   <div class="grid-item">8</div>
+//   <div class="grid-item">9</div>  
+// </div>
+
+
+
+}
